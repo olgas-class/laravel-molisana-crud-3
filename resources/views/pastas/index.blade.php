d901ea995f48c032af2f3f1bad84580ea6c8c6d7
olga_demina <olga.demina1410@gmail.com>
2024-06-14 12:58:00 +0200

    feat: link to index page to create pasta

--- resources/views/pastas/index.blade.php
+++ resources/views/pastas/index.blade.php
@@ -1,30 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>
-            Lista delle paste
-        </h1>
+  <div class="container">
+    <h1>
+      Lista delle paste
+    </h1>
 
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">id</th>
-                    <th scope="col">Titolo</th>
-                    <th scope="col">Tipologia</th>
-                    <th scope="col">Azioni</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($listaPasta as $item)
-                    <tr>
-                        <th scope="row">{{ $item->id }}</th>
-                        <td>{{ $item->title }}</td>
-                        <td>{{ $item->type }}</td>
-                        <td><a href="{{route('pastas.show',['pasta'=>$item->id])}}">Dettagli</a></td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+    <a href="{{ route('pastas.create') }}">Crea una nuova pasta</a>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">id</th>
+          <th scope="col">Titolo</th>
+          <th scope="col">Tipologia</th>
+          <th scope="col">Azioni</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($listaPasta as $item)
+          <tr>
+            <th scope="row">{{ $item->id }}</th>
+            <td>{{ $item->title }}</td>
+            <td>{{ $item->type }}</td>
+            <td><a href="{{ route('pastas.show', ['pasta' => $item->id]) }}">Dettagli</a></td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
 @endsection
