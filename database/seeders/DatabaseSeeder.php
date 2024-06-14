e3d8fd51dc7d2db5873c5c0998c40c2a6f0a22b8
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-06-14 12:01:46 +0200

    feat: added call in run

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        //grazie a questo non bisogna specificare la classe nel terminale al run del seeder
+        $this->call([
+            PastasTableSeeder::class
+        ]);
     }
 }
