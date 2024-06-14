0e56c101a1820482c063003823725d1259d0f026
olga_demina <olga.demina1410@gmail.com>
2024-06-14 12:57:31 +0200

    add: fillable to model

--- app/Models/Pasta.php
+++ app/Models/Pasta.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Pasta extends Model
 {
     use HasFactory;
+
+    protected $fillable = ["title", "type", "cooking_time", "weight", "description", "image"];
 }
